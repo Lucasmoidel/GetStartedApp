49760daaf6af24746200f28b8d87b4f98f2148ff
Lucasmoidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-06-23 17:27:36 -0700

    Add project files.

---
+++ GetStartedApp/Views/MainView.axaml.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
+using System.Net.Security;
+
+namespace GetStartedApp.Views;
+
+public partial class MainView : UserControl
+{ 
+    public MainView()
+    {
+        InitializeComponent();
+    }
+    //public bool CanResize = false;
+    public string HelloText => "Hello world!!!";
+    public void NoButtonClicked(object source, RoutedEventArgs args)
+    {
+        Debug.WriteLine("Click!");
+    }
+
+    public void YesButtonClicked(object source, RoutedEventArgs args)
+    {
+
+    //Debug.WriteLine("Click!");
+    }
+}
